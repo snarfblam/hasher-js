c3132f548e2cc6c9dddce3bf2625cfe0279c6127
Tom H <snarfblam@gmail.com>
2018-09-10 22:34:37 -0400

    Hasher now performs all hashes in single pass (ROM is streamed once and all hashes calc'd at same time)

--- src/RomHasher.js
+++ src/RomHasher.js
@@ -26,6 +26,9 @@ class RomHasher {
         /** @type {{algoName: string, region: RomRegion}[]} */
         this.hashTasks = [];
 
+        /** @type {{algoNames: string[], region: RomRegion}[]} */
+        this.hashTaskBuckets = [];
+
         this._parseHashlist();
         this._queueHashTasks();
         this._cancel = false;
@@ -64,6 +67,33 @@ class RomHasher {
                 algoName: item.algoName,
             });
         });
+
+        var hashlist = this.hashlist.slice();
+        var hashBuckets = [];
+
+        // Split hash items into buckets where each item within a bucket differs only by algorithm
+        while (hashlist.length > 0) {
+            // We'll separate all hash items that have the same region as the first remaining item
+            var region = hashlist[0].region;
+            var predicate =
+                ( /** @type {{algoName: string, region: RomRegion, value: string}} */ item) =>
+                    region.isSameRegion(item.region);
+
+            var [bucket, remainder] = partition(hashlist, predicate);
+            // Separated items go into a bucket
+            hashBuckets.push(bucket);
+            // Remaining items will be processed in same manner
+            hashlist = remainder;
+        }
+
+        // reduce separate items to single item with array of algorithms
+        this.hashTaskBuckets = hashBuckets.map(bucket => {
+            var algoList = bucket.map(hashItem => hashItem.algoName);
+            return {
+                algoNames: algoList,
+                region: bucket[0].region
+            }
+        });
     }
 
     /** Initiates the hashing process. Returns a promise.
@@ -72,31 +102,65 @@ class RomHasher {
     performHashes() {
         var progressList = [];
 
-        var hashPromises = this.hashTasks.map(task => {
-            /** @type {function(): Promise<string>} */
-            var algoFunc = hasher[task.algoName + 'Async'];
-            if (!algoFunc) return Promise.reject("Hash algorithm " + task.algoName + " is not available");
+        var startTime = 0;
+        if (performance && performance.now) startTime = performance.now();
+
+        // var hashPromises = this.hashTasks.map(task => {
+        //     /** @type {function(): Promise<string>} */
+        //     var algoFunc = hasher[task.algoName + 'Async'];
+        //     if (!algoFunc) return Promise.reject("Hash algorithm " + task.algoName + " is not available");
     
-            var rom = task.region.rom;
+        //     var rom = task.region.rom;
+        //     if (rom instanceof Rom) rom = rom.file;
+
+        //     // If the hash operation is cancelled, skip over any pending hashes
+        //     if (this._cancel) return Promise.resolve(null);
+        //     var progressEntryIndex = progressList.length;
+        //     progressList.push(0);
+        //     var progressCallback = (amt) => {
+        //         progressList[progressEntryIndex] = amt;
+        //         if (this._progressCallback) {
+        //             var totalProgress = progressList.reduce((a, v) => v + a, 0) / this.hashTasks.length;
+        //             this._progressCallback(totalProgress);
+        //         }
+        //     }
+
+        //     var hashPromise = algoFunc(rom, task.region.offset, task.region.length, progressCallback);
+        //     var hashDonePromise = hashPromise.then(hash => {
+        //         var matches = this.hashlist.filter(item => item.region.isSameRegion(task.region) && item.algoName === task.algoName);
+        //         if (matches.length === 0) console.warn("task to result error");
+        //         matches.forEach(match => match.value = hash || match.value);
+        //     });
+            
+        //     // Keep references to the cancel function for each hash operation so we can abort
+        //     this._cancelList.push(hashPromise.cancel);
+        //     return hashDonePromise;
+        // });
+
+        var hashPromises = this.hashTaskBuckets.map(bucket => {
+            var rom = bucket.region.rom;
             if (rom instanceof Rom) rom = rom.file;
 
             // If the hash operation is cancelled, skip over any pending hashes
             if (this._cancel) return Promise.resolve(null);
-            var progressEntryIndex = progressList.length;
+            var progressListIndex = progressList.length;
             progressList.push(0);
             var progressCallback = (amt) => {
-                progressList[progressEntryIndex] = amt;
+                progressList[progressListIndex] = amt;
                 if (this._progressCallback) {
-                    var totalProgress = progressList.reduce((a, v) => v + a, 0) / this.hashTasks.length;
+                    var totalProgress = progressList.reduce((a, v) => v + a, 0) / progressList.length;
                     this._progressCallback(totalProgress);
                 }
             }
 
-            var hashPromise = algoFunc(rom, task.region.offset, task.region.length, progressCallback);
-            var hashDonePromise = hashPromise.then(hash => {
-                var matches = this.hashlist.filter(item => item.region.isSameRegion(task.region) && item.algoName === task.algoName);
-                if (matches.length === 0) console.warn("task to result error");
-                matches.forEach(match => match.value = hash || match.value);
+            var hashPromise = hasher.hashMultiAsync(bucket.algoNames, rom, bucket.region.offset, bucket.region.length, progressCallback);
+            var hashDonePromise = hashPromise.then(hashResults => {
+                for (var i = 0; i < hashResults.length; i++) {
+                    var algoName = bucket.algoNames[i];
+                    var matches = this.hashlist.filter(item => item.region.isSameRegion(bucket.region) && item.algoName === algoName);
+                    if (matches.length === 0) console.warn("bucket to result error");
+                    matches.forEach(match => match.value = hashResults[i] || match.value);
+                }
             });
             
             // Keep references to the cancel function for each hash operation so we can abort
@@ -106,6 +170,12 @@ class RomHasher {
 
         return Promise.all(hashPromises)
             .then(() => {
+                if (performance && performance.now) {
+                    var endTime = performance.now();
+                    console.log("Hashing operation complete in " + ((endTime - startTime) / 1000) + "s");
+                }
+
+
                 return this.hashlist;
             });
     }
@@ -118,5 +188,26 @@ class RomHasher {
     }
 
 }
+
+/** Splits an array into a "matched" and "non-matched" array based on a predicate
+ *  @param {T[]} array
+ *  @param {function(T): boolean} predicate 
+ *  @returns {[T[], T[]]}
+ *  @template T
+ */
+function partition(array, predicate) {
+    var match = [];
+    var nonmatch = [];
+    array.forEach(item => {
+        if (predicate) {
+            match.push(item);
+        } else {
+            nonmatch.push(item);
+        }
+    });
+
+    return [match, nonmatch];
+}
+
 
 export default RomHasher;
--- src/hash/index.js
+++ src/hash/index.js
@@ -17,6 +17,17 @@ import crc32 from './crc32';
 import jsSha1 from 'js-sha1';
 import jsMd5 from 'js-md5';
 
+/** An object that can be indexed to get Hasher objects associated with a string representation
+ *  @type {{[x: string]: Hasher}}
+ */
+var algoMap = {
+    crc16: crc16,
+    crc32: crc32,
+    // @ts-ignore
+    sha1: jsSha1,
+    md5: jsMd5
+};
+
 /** The chunk size used */
 const chunkSize = 0x4000;
 
@@ -117,6 +128,92 @@ function hashAsync(algo, buffer, offset, length, progressCallback) {
     return resultPromise;
 }
 
+
+/**
+ * 
+ * @param {(Hasher | string)[]} algos An object that implements the appropriate digesting interface (create() method which produces an object with update() and hex() methods)
+ * @param {Blob | Uint8Array} buffer 
+ * @param {number} offset 
+ * @param {number} length 
+ * @param {function(number): void} [progressCallback]
+ * @returns {Promise<string[]>}
+ */
+function hashMultiAsync(algos, buffer, offset, length, progressCallback) {
+    var end = offset + length;
+    var hasherObjets = algos = algos.map(algo => {
+        if (typeof algo === 'string') {
+            var result = algoMap[algo];
+            if (!result) throw Error('Unknown algorithm');
+            return result;
+        } else {
+            return algo;        }
+    });
+    var hashers = hasherObjets.map(algo => algo.create());
+
+    // Convert byte array to a blob
+    if (buffer instanceof Uint8Array) {
+        buffer = asBlob(buffer, offset, length);
+        offset = 0;
+    };
+
+    /** When set to true, will abort the hash operation */
+    var cancel = false;
+    var processedByteCount = 0;
+    /** @type {Promise & {cancel?: function(): void}} */
+    var resultPromise = new Promise((resolve, reject) => {
+        if (buffer instanceof Blob) {
+            end = Math.min(end, buffer.size);
+
+            var blob = buffer;
+            var reader = new FileReader();
+            var currentOffset = offset;
+            var readNextChunk = () => {
+                if (currentOffset < end) {
+                    if (cancel) {
+                        resolve(null);
+                        return;
+                    }
+
+                    // don't include anything beyond end of blob in length
+                    var currentEnd = Math.min(currentOffset + chunkSize, end);
+                    reader.readAsArrayBuffer(blob.slice(currentOffset, currentEnd));
+                    currentOffset += chunkSize;
+                } else {
+                    // We've finished processing the file
+                    resolve(hashers.map(hasher => hasher.hex()));
+                }
+            };
+            var dataReady = () => {
+                // @ts-ignore
+                var data = new Uint8Array(reader.result);
+                //hasher.update(data);
+                hashers.forEach(hasher => hasher.update(data));
+                processedByteCount += data.length;
+                if (progressCallback) progressCallback(processedByteCount / length);
+
+                // @ts-ignore
+                if (window.poopMode) {
+                    // IE will freeze if we don't explicitly yield the CPU
+                    setTimeout(readNextChunk, 1);
+                } else {
+                    readNextChunk();
+                }
+            };
+
+            reader.onload = dataReady;
+            reader.onerror = () => { reject(reader.error || Error("Unknown error reading a file")) };
+
+            // start it off
+            readNextChunk();
+        } else {
+            reject(Error('unknown input type'));
+        }
+    });
+
+    resultPromise.cancel = () => cancel = true;
+    return resultPromise;
+}
+
 /**
  * Promise. Resolves to a hex string containing the MD5 digest of the buffer.
  * @param {Uint8Array} buffer 
@@ -167,4 +264,5 @@ function crc16Async(buffer, offset, length, progressCallback) {
 function crc32Async(buffer, offset, length, progressCallback) {
     return hashAsync(crc32, buffer, offset, length, progressCallback);
 }
-export { jsSha1 as sha1, crc16, crc32, sha1Async, crc16Async, md5Async, crc32Async };
+
+export { jsSha1 as sha1, crc16, crc32, sha1Async, crc16Async, md5Async, crc32Async , hashMultiAsync};
