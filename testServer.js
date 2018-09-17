49588fa430c4979e06bc8717edb476bca67b7eca
Tom H <snarfblam@gmail.com>
2018-09-16 21:43:27 -0400

    Added 'can't find build output' message to test server and updated 'npm start'

--- testServer.js
+++ testServer.js
@@ -5,19 +5,24 @@
 */
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
 
 const PORT = 8000;
 
-var app = express();
+if (fs.existsSync(path.join(__dirname, 'dist', 'hasher.js'))) {
+    var app = express();
 
-// localhost/test/...
-const testDir = path.join(__dirname, '/test');
-console.log(testDir);
-app.use('/test', express.static(testDir));
-const distDir = path.join(__dirname, '/dist');
-// localhost/...
-app.use(express.static(distDir));
+    // localhost/test/...
+    const testDir = path.join(__dirname, '/test');
+    app.use('/test', express.static(testDir));
 
-app.listen(PORT , function () {
-    console.log("listening on " + PORT);
-})
+    // localhost/...
+    const distDir = path.join(__dirname, '/dist');
+    app.use(express.static(distDir));
+
+    app.listen(PORT, function () {
+        console.log("listening on " + PORT);
+    })
+} else {
+    console.log("Can not find build output. Make sure you 'npm run build' before you 'npm start'.");
+}
